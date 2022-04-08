9c7d6a84839e56f8b1dbc2da0b9568d249355063
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 13:47:08 -0700

    Rippler

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
