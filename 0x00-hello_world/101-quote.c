d71559ccb4e5982f2643be88c503c3b52d76ce7a
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 03:45:32 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - Function begins
+ * Return: Function ends
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
