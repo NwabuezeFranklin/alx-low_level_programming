f95f8734d080ea8b5eb4f797610125b7501c3b97
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 13:52:47 -0700

    Ripple effect

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
