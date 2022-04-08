3af587b32a5f644ff0c45b444aac4b480b4b79ad
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 13:10:55 -0700

    Panda nah boss

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
