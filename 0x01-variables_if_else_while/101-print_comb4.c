0b4aa0d047b144ca75543ec91163e2e697eecae4
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 15:13:23 -0700

    Woto

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
