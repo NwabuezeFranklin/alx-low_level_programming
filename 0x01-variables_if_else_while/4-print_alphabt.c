c5f7f9649b5d19086a36e057188f5baa6845b4a8
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 13:37:49 -0700

    ripple

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i ! = 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
