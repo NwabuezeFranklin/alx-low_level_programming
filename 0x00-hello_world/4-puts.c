dcd69345f0cbca2708fcdfe6afb3fa7a463adeb0
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-07 20:19:32 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - function starts
+ *
+ * Return: function closes
+ */
+
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
