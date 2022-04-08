7b095dd7a85f26417f652381aa29ab0b80a6eb34
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 12:05:29 -0700

    Panda

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,17 +15,17 @@ int main(void)
 
 	if (n == 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 
 	else if (n < 0)
 	{
-		printf("%1 is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	else
 	{
-		print f("%i is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
