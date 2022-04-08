a63a7362dfe67ec60fad91bfa227ad59609801cc
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 15:41:11 -0700

    how

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 	int i;
 	int j;
 
-	for (i =48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			if(i != j && i < j)
+			if (i != j && i < j)
 			{
 				putchar(i);
 				putchar(j);
