cb33e5393a6638e587920ae30671dd62568f82a5
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-08 14:56:14 -0700

    gone

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i =48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if(i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (j == 57 && i == 56)
+				{
+					break
+						;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
