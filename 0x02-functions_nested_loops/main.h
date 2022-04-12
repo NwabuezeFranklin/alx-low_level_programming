b5c7336ef42968fdebbf89c1630e2867cd771778
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-12 13:37:48 -0700

    hope

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
+
+
