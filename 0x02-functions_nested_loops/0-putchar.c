0d45abea0e48c73c20446e7122e04e04055cc77b
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-12 14:53:43 -0700

    fine

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,45 +1,22 @@
 #include "main.h"
 
-
-
 /**
+ * Main - Entry point
  *
- * *main - Entry point
- *
- * *
- *
- * *Return: nothing, return void
- *
- * */
-
-
+ * Return: nothing, return void
+ */
 
 int main(void)
 
-
-
 {
-
 	  _putchar('_');
-
-	    _putchar('p');
-
-	      _putchar('u');
-
-	        _putchar('t');
-
-		  _putchar('c');
-
-		    _putchar('h');
-
-		      _putchar('a');
-
-		        _putchar('r');
-
-			  _putchar('\n');
-
-
-
-			    return (0);
-
+	  _putchar('p');
+	  _putchar('u');
+	  _putchar('t');
+	  _putchar('c'); 
+	  _putchar('h');
+          _putchar('a');
+	  _putchar('r');
+	  _putchar('\n');
+          return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * print_alphabet - entry point
+ *
+ * * Return: 0
+ *
+ * */
+
+
+
+void print_alphabet(void)
+
+{
+
+	  char c = 'a';
+
+	    
+
+	    while (c <= 'z')
+
+		      {
+
+			          _putchar(c);
+
+				      c++;
+
+				        }
+
+	      _putchar('\n');
+
+	        
+
+}
