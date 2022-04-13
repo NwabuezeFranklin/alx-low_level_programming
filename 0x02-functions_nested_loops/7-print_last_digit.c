91a1a77804e71747944d1dfcfa2ec930047d92c1
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-13 02:51:41 -0700

    e

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,14 +15,14 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		last_digit = (-1 * (n % 10));
-		_putschar (last_digit + '0');
+		_putchar (last_digit + '0');
 		return (last_digit);
 	}
 
 	else
 	{
 		last_digit = (n % 10);
-		_putschar (last_digit + '0');
+		_putchar (last_digit + '0');
 		return (last_digit);
 	}
 
