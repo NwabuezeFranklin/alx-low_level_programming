d83da450e648e8a1fc24b925e3f10027bc7dda38
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-12 18:45:08 -0700

    n

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Return: nothing, return void
+*/
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
