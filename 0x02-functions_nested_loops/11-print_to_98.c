d858d64e7a8ec0eba8815c53de2a356db0b444fe
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-12 21:57:30 -0700

    j

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,17 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (n = n; n < 98; n++)
+		{
 			printf("%d, ", n);
 			printf("%d\n", 98);
+		}
 	}
 	else
 	{
 		for (n = n; n > 98; n--)
+		{
 			printf("%d, ", n);
 			printf("%d\n", 98);
+		}
 	}
 }
