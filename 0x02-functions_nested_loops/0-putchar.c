82477bdac36d2923e0d52145245783b8d058dcdd
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-12 18:04:43 -0700

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,16 @@
  */
 
 int main(void)
-
 {
-	  _putchar('_');
-	  _putchar('p');
-	  _putchar('u');
-	  _putchar('t');
-	  _putchar('c'); 
-	  _putchar('h');
-          _putchar('a');
-	  _putchar('r');
-	  _putchar('\n');
-          return (0);
+	_putschar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	return (0);
 }
