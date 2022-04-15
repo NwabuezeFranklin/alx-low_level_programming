31ab03411444f9adf33c21e1e12761ad0824f93f
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-14 20:37:26 -0700

    ky

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print prime
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
