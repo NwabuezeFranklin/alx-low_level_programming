1dc6d5cc569616dafcfb9dcb11fbadd267808363
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-14 20:21:32 -0700

    jo

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
