f44b9c496f07db326a009e16b0e7f50e0cd55e5d
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-20 19:41:38 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - reverse something
+ * @s: params
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
