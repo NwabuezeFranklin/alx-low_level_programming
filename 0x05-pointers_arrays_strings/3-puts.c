0195b225de3e59704b6460d60f4f62a49afd8129
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-20 19:40:12 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - pusts to stdout
+ * @str: params
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
