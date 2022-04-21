5161f653c0dc502233dea6dcef7d151c3f053c4b
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-20 19:45:36 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - skips 2
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
