1ccf8e7ee881239b12d0768503ccd96b9761ce9d
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-04-23 04:56:33 -0700

    main file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char);
