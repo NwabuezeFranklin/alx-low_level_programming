00acb42f1a1d5c8a861c1d1910ec011b59a62006
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-02 21:31:59 -0700

    OK

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,24 +1,2 @@
-es (20 sloc)  348 Bytes
-#include "main.h"
-
-/**
- * _isupper - A function that checks for uppercase character.
- * @c: An input character
- * Return: 1 if c is uppercase or 0 otherwise
- */
-int _isupper(int c)
-{
-	char uppercase = 'A';
-	int isupper = 0;
-
-	for (; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-
-	return (isupper);
-}
+#!/bin/bash
+int _isupper(int c);
