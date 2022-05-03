8ea467a4c2d677526bc8c690e828fba9b396e75d
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-02 17:00:15 -0700

    Write a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+printf("%i\n", argc - 1);
+
+return (0);
+}
