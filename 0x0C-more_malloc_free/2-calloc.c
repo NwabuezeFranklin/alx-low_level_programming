70f5285f859214c3de94ea71ceae89324ce3aca5
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-09 04:40:08 -0700

    fixed task calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,39 @@
-include "main.h"
+#include "main.h"
 
 /**
- *_calloc -allocated memoria for nmeb elemn de zise bytes
- *@nmemb: number of element in the array
- *@size: bytes for each position in array
- *Return: pointer void
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *p;
-unsigned int i;
+	void *p;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
 
-p = malloc(nmemb * size);
-if (p == NULL)
-return (NULL);
-
-for (i = 0; i < nmemb * size; i++)
-p[i] = 0;
-
-return (p);
-
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
