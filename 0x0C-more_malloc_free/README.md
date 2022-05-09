fef0864c88523fdc4d46a89ce6561c2d00ca8afd
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-09 02:52:45 -0700

    Initials

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+How to use the exit function
+What are the functions calloc and realloc from the standard library and how to use them
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmedb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
