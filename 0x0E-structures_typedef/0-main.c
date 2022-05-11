06f603a682a4eefc6c571236e527a6b13f772de4
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-11 08:30:10 -0700

    Necessary

---
+++ 0x0E-structures_typedef/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
