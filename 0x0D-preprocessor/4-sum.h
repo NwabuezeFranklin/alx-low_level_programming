631dd1bac31fdc3fa3534b0523b964b28b52ea3c
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-11 10:04:51 -0700

    faster..

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,27 +2,3 @@
 #define _SUM_X_
 #define SUM(x, y) ((x) + (y))
 #endif
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-"4-sum.h" [New File]                                                            0,0-1         All
-
