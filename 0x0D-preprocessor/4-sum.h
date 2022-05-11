374c24f4bea5ceb6b29e78cd0fe8ac249a3a8a78
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-11 06:54:16 -0700

    Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,28 @@
+#ifndef add
+#define add
+#define SUM(x, y) ((x) + (y))
+#endif
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"4-sum.h" [New File]                                                            0,0-1         All
+
