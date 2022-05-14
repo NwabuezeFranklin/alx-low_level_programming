0d487f9d2361ea765da3ac4006ed795f5be23b10
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-05-13 17:00:34 -0700

    Necessities

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+General
+What are function pointers and how to use them
+What does a function pointer exactly hold
+Where does a function pointer point to in the virtual memory
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
