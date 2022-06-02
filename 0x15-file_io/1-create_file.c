856c75976b3ab494a222d981ba6b8d95748b0866
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-06-01 19:23:36 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
