776320eab8c5973042b0ba1c4939638d2aad2c9e
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-07-12 20:30:01 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int add(int a,int b)
+{
+	return a + b;
+}
+
+int sub(int a,int b)
+{
+        return a - b;
+}
+
+int mul(int a,int b)
+{
+        return a * b;
+}
+
+
+int div(int a,int b)
+{
+        return a / b;
+}
+
+int mod(int a,int b)
+{
+        return a % b;
+}
+
