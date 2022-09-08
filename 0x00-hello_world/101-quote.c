6b8ef2315ef9bc1ced07402ad7c05840f1748135
Nwabueze Franklin <99493414+NwabuezeFranklin@users.noreply.github.com>
2022-09-08 15:02:41 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Function begins
- * Return: Function ends
+ * main - prints exactly "and that piece of art is useful"
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
